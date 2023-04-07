239713a1ec80ca7fea524a03d31334067ff3c812
JacobYaws <Jacob.Yaws@du.edu>
2023-04-07 01:35:06 -0600

    Changed the mongodb back to the local IP address

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/bookstore",
+  // process.env.MONGODB_URI || "mongodb://localhost:27017/bookstore",
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/bookstore",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
