0b6de6c9435ff3b2585c1f70ce7d1cbb3a55e075
JacobYaws <Jacob.Yaws@du.edu>
2023-04-07 20:51:19 -0600

    Testing api call in index.html. Changed Contact.js back to original

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,136 +1,137 @@
-import React, { useState } from 'react';
+import { Button, Form, Input } from 'antd';
+const onFinish = (values) => {
+  console.log('Success:', values);
+};
+const onFinishFailed = (errorInfo) => {
+  console.log('Failed:', errorInfo);
+};
+const ContactFormPage = () => (
+  <Form 
+    name="basic"
+    labelCol={{
+      span: 8,
+    }}
+    wrapperCol={{
+      span: 16,
+    }}
+    style={{
+      maxWidth: '50%',
+      margin: '0 18% 0 18%'
+    }}
+    initialValues={{
+      remember: true,
+    }}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete="off"
+  >
+    <Form.Item
+      label="Your name"
+      name="yourName"
+      rules={[
+        {
+          required: true,
+          message: 'Please input your username!',
+        },
+      ]}
+    >
+      <Input />
+    </Form.Item>
 
-function ContactFormPage() {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
-  });
+    <Form.Item
+      label="Your e-mail address"
+      name="email"
+      rules={[
+        {
+          required: true,
+          message: 'Please input your password!',
+        },
+      ]}
+    >
+      <Input />
+    </Form.Item>
 
-  function handleChange(event) {
-    setFormData({
-      ...formData,
-      [event.target.name]: event.target.value,
-    });
-  }
+    <Form.Item
+      label="Subject"
+      name="subject"
+      rules={[
+        {
+          required: true,
+          message: 'Please input your password!',
+        },
+      ]}
+    >
+      <Input />
+    </Form.Item>
+    <Form.Item
+      label="Message"
+      name="message"
+      rules={[
+        {
+          required: true,
+          message: 'Please input your password!',
+        },
+      ]}
+    >
+      <Input />
+    </Form.Item>
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    console.log(formData);
-  }
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name:
-          <input type="text" name="name" value={formData.name} onChange={handleChange} />
-        </label>
-        <label>
-          Email:
-          <input type="email" name="email" value={formData.email} onChange={handleChange} />
-        </label>
-        <label>
-          Message:
-          <textarea name="message" value={formData.message} onChange={handleChange} />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
-}
+    <Form.Item
+      wrapperCol={{
+        offset: 8,
+        span: 16,
+      }}
+    >
+      <Button type="primary" htmlType="submit">
+        Send Message
+      </Button>
+    </Form.Item>
+  </Form>
+);
 
 export default ContactFormPage;
 
-// import { Button, Form, Input } from 'antd';
-// const onFinish = (values) => {
-//   console.log('Success:', values);
-// };
-// const onFinishFailed = (errorInfo) => {
-//   console.log('Failed:', errorInfo);
-// };
-// const ContactFormPage = () => (
-//   <Form 
-//     name="basic"
-//     labelCol={{
-//       span: 8,
-//     }}
-//     wrapperCol={{
-//       span: 16,
-//     }}
-//     style={{
-//       maxWidth: '50%',
-//       margin: '0 18% 0 18%'
-//     }}
-//     initialValues={{
-//       remember: true,
-//     }}
-//     onFinish={onFinish}
-//     onFinishFailed={onFinishFailed}
-//     autoComplete="off"
-//   >
-//     <Form.Item
-//       label="Your name"
-//       name="yourName"
-//       rules={[
-//         {
-//           required: true,
-//           message: 'Please input your username!',
-//         },
-//       ]}
-//     >
-//       <Input />
-//     </Form.Item>
+// import React, { useState } from 'react';
 
-//     <Form.Item
-//       label="Your e-mail address"
-//       name="email"
-//       rules={[
-//         {
-//           required: true,
-//           message: 'Please input your password!',
-//         },
-//       ]}
-//     >
-//       <Input />
-//     </Form.Item>
+// function ContactFormPage() {
+//   const [formData, setFormData] = useState({
+//     name: '',
+//     email: '',
+//     message: '',
+//   });
 
-//     <Form.Item
-//       label="Subject"
-//       name="subject"
-//       rules={[
-//         {
-//           required: true,
-//           message: 'Please input your password!',
-//         },
-//       ]}
-//     >
-//       <Input />
-//     </Form.Item>
-//     <Form.Item
-//       label="Message"
-//       name="message"
-//       rules={[
-//         {
-//           required: true,
-//           message: 'Please input your password!',
-//         },
-//       ]}
-//     >
-//       <Input />
-//     </Form.Item>
+//   function handleChange(event) {
+//     setFormData({
+//       ...formData,
+//       [event.target.name]: event.target.value,
+//     });
+//   }
 
-//     <Form.Item
-//       wrapperCol={{
-//         offset: 8,
-//         span: 16,
-//       }}
-//     >
-//       <Button type="primary" htmlType="submit">
-//         Send Message
-//       </Button>
-//     </Form.Item>
-//   </Form>
-// );
+//   function handleSubmit(event) {
+//     event.preventDefault();
+//     console.log(formData);
+//   }
+
+//   return (
+//     <div>
+//       <form onSubmit={handleSubmit}>
+//         <label>
+//           Name:
+//           <input type="text" name="name" value={formData.name} onChange={handleChange} />
+//         </label>
+//         <label>
+//           Email:
+//           <input type="email" name="email" value={formData.email} onChange={handleChange} />
+//         </label>
+//         <label>
+//           Message:
+//           <textarea name="message" value={formData.message} onChange={handleChange} />
+//         </label>
+//         <button type="submit">Submit</button>
+//       </form>
+//     </div>
+//   );
+// }
 
 // export default ContactFormPage;
+
